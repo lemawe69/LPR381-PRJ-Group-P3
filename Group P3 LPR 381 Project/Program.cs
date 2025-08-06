1c2504ff9ca29ab126fabbfdb0d232e8400bcb7d
lemawe69 <brandonlemmer777@gmail.com>
2025-08-06 21:37:41 +0200

    File Structure

---
+++ Group P3 LPR 381 Project/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Group_P3_LPR_381_Project
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
